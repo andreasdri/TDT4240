d621189d4faa6aafe62e96418f0daf6dc9cd4a38
Stein-Otto Svorstøl <steinotto.svorstol@gmail.com>
2015-02-02 23:56:44 +0100

    Removed coordinates and fps from task 1.

--- Task 1 - Sprites/app/src/main/java/com/example/andybb/myapplication/GameState.java
+++ Task 1 - Sprites/app/src/main/java/com/example/andybb/myapplication/GameState.java
@@ -16,25 +16,21 @@ import sheep.game.State;
 public class GameState extends State  {
     private World gameWorld;
     private GameLayer gameLayer;
-    private DebugInfo debugInfo;
 
     public GameState() {
         gameWorld = new World();
         gameLayer = new GameLayer();
         gameWorld.addLayer(gameLayer);
-        debugInfo = new DebugInfo(gameLayer);
     }
 
     @Override
     public void draw(Canvas canvas) {
         canvas.drawColor(Color.BLACK);
         gameWorld.draw(canvas);
-        debugInfo.draw(canvas);
     }
 
     @Override
     public void update(float dt) {
         gameWorld.update(dt);
-        debugInfo.update(dt);
     }
 }
